ff81587ad2aa3907720d8f13544cc09cb4c35b59
André Araujo <aaraujo1@my.wctc.edu>
2019-10-16 13:21:31 -0500

    method to load image and added statements for images 1, 2, and 3

--- Photo Booth/123.py
+++ Photo Booth/123.py
@@ -3,12 +3,30 @@ import pygame
 def main():
 	running = True
 	while running:
-		# event handling
+		# event handling, gets all event from the queue
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT or (event.type == pygame.KEYUP and event.key == pygame.K_ESCAPE):
 				running = False
+			# if spacebar is pressed
+			elif event.type == pygame.KEYUP and event.key == pygame.K_SPACE:
+				# img = pygame.image.load('123.png') 
+				# screen.blit(img, (0,0))
+				# pygame.display.update()
+				load_img('123')
+			elif event.type == pygame.KEYUP and event.key == pygame.K_1:
+				load_img('1')
+			elif event.type == pygame.KEYUP and event.key == pygame.K_2:
+				load_img('2')
+			elif event.type == pygame.KEYUP and event.key == pygame.K_3:
+				load_img('3')
+
+# method to load image based on key
+def load_img(n):
+	img = pygame.image.load('{0}.png'.format(n))
+	screen.blit(img, (0, 0))
+	pygame.display.update()		
 		
-		
+# method to quit
 def destroy():
 	print("cleanup")
 	pygame.quit()
