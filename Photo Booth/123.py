a1edaae7e4fc2f1efbf81baa5e73c38688f4cdb9
André Araujo <aaraujo1@my.wctc.edu>
2019-10-16 13:14:45 -0500

    work on main method

--- Photo Booth/123.py
+++ Photo Booth/123.py
@@ -1,6 +1,25 @@
 import pygame
 
-if _name_ == '__main__' : # Program start
+def main():
+	running = True
+	while running:
+		# event handling
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT or (event.type == pygame.KEYUP and event.key == pygame.K_ESCAPE):
+				running = False
+		
+		
+def destroy():
+	print("cleanup")
+	pygame.quit()
+
+
+if __name__ == '__main__' : # Program start
 	pygame.init()
 	pygame.display.set_caption("1-2-3")
+	screen = pygame.display.set_mode((400,400))
+	try:
+		main()
+	except KeyboardInterrupt: # when ctrl+c is pressed
+		destroy()
 	
